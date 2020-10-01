c77846c6b7bff2cc83c5e7d71c862b8bce7d8149
KwonBoKyeung <kbg514@naver.com>
2020-10-01 15:09:41 +0900

    경로 찾기

--- BOJ/11403.cpp
+++ BOJ/11403.cpp
@@ -3,47 +3,44 @@
 
 using namespace std;
 
-int map[101][101];
-int visit[101][101];
-int N;
-
-void dfs(int x)
-{
-	for(int i=0; i<N; i++)
-	{
-		if(map[x][i] == 1 && visit[x][i] == 0)
-		{
-			visit[x][i] = 1;
-			dfs(i);
-		}
-	}
-}
+int d[101][101];
 
 int main()
 {
+	int N;
 	cin >> N;
 
 	for(int i=0; i<N; i++)
 	{
 		for(int j=0; j<N; j++)
 		{
-			cin >> map[i][j];
+			cin >> d[i][j];
 		}
+	}
+
+
+	for (int k = 0; k < N; ++k) {
+
+    	for (int i = 0; i < N; ++i) {
+
+	        for (int j = 0; j < N; ++j) {
+
+	            if (d[i][k] == 1 && d[k][j] == 1) {
+	                d[i][j] = 1;
+	            }
+
+        	}
+    	}
 	}
 
 	for(int i=0; i<N; i++)
 	{
-		memset(visit, 0, sizeof(visit));
-		dfs(i);
-
 		for(int j=0; j<N; j++)
 		{
-			cout << visit[i][j] << " ";
+			cout << d[i][j] << " ";
 		}
-		cout << endl;
-
+		cout << "\n";
 	}
-
 
 	return 0;
 
